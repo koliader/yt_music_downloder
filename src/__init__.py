d96b4d5de2e4a86116280e0e1e09cb9bbaccc8f2
Mike Derevyankon <mderevyanko@hotmail.com>
2023-11-18 16:47:52 +0300

    calean version

--- src/__init__.py
+++
@@ -1,4 +0,0 @@
-from .get_link import *
-from .parse_playlist import *
-from .user_data_disp import UserDataDispatcher
-from .validator import Validator
--- src/user_data_disp.py
+++
@@ -1,7 +0,0 @@
-class UserDataDispatcher:
-    # Get data from user
-    @staticmethod
-    def get_user_data():
-        playlist_link = input("Enter playlist link to save: ")
-        output_directory = input("Enter output directory: ")
-        return {"link": playlist_link, "directory": output_directory}
